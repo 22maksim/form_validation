ed5d7b6e8f4d019bc91026315d0d18923ab1a84f
22maksim <15maksim@mail.ru>
2023-08-27 22:29:49 +0300

    validation useForm and yup

--- src/App.js
+++ src/App.js
@@ -1,88 +1,50 @@
-import { useRef, useState } from 'react';
 import './App.css';
-import { useStore } from './hook/useStore';
-import { searchErrorsPasword } from './core/searchErrorsPasword';
-import { dataServer } from './core/dataServer';
+import { useForm, } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup';
+import fieldsSchema from './core/fieldSchema';
 
 function App() {
-	const { getState, updateState } = useStore()
-	const [errorsInput, setErrorsInput] = useState(null)
-	const { email, password, passwordConfirm } = getState()
-	const refButton = useRef(null)
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			passwordConfirm: '',
+		},
+		resolver: yupResolver(fieldsSchema)
+	})
 
-	const onSubmit = (event) => {
-		event.preventDefault()
-		if (password === passwordConfirm && !!email && password !== '') {
-			dataServer(getState())
-			setErrorsInput(null)
-		} else if (email === '') {
-			setErrorsInput('Не указан email.')
-		} else if (password !== passwordConfirm) {
-			setErrorsInput('Пароли не равны')
-		}
-	}
+	const passwordError = errors.password?.message
+	const passwordConfirmError = errors.passwordConfirm?.message
+	const emailError = errors.email?.message
 
-	const onChange = ({ target }) => {
-		setErrorsInput(null)
-		const name = target.name
-		const value = target.value
-		updateState(name, value)
-		if (name === 'password' || name === 'passwordConfirm') {
-			setErrorsInput(searchErrorsPasword(value))
+	const onSubmit = (data, e) => {
+		if (isValid) {
+			console.log(data, e)
 		}
-		if (password === value && password.length > 4 && !!email) {
-			refButton.current.focus()
-		}
-	}
-
-	const onPasswordBlur = () => {
-		if (password.length < 5) {
-			setErrorsInput('длинна пароля должна быть больше 4')
-		}
-	}
-
-	const onBlurPasswordConfirm = () => {
-		if (passwordConfirm < 5) {
-			setErrorsInput('длинна пароля должна быть больше 4')
-		}
+		reset()
 	}
 
 	return (
 		<div className="App">
-			<form className='form' onSubmit={onSubmit}>
-				<input
-					type="email"
-					name='email'
-					value={email}
-					onChange={onChange}
-					placeholder='email'
-					onBlur={onPasswordBlur}
+			<form className='form' onSubmit={handleSubmit(onSubmit)}>
+				<input placeholder='email' name='email' type="email" {...register('email')} />
+				{emailError && <div className='error'>{emailError}</div>}
 
-				/>
-				<input
-					type="password"
-					name='password'
-					value={password}
-					onChange={onChange}
-					placeholder='password'
-					onBlur={onPasswordBlur}
-				/>
-				<input
-					type="password"
-					name='passwordConfirm'
-					value={passwordConfirm}
-					onChange={onChange}
-					placeholder='return password'
-					onBlur={onBlurPasswordConfirm}
-				/>
-				<button ref={refButton} disabled={!!errorsInput}>Зарегистрироваться</button>
+				<input ref={register} placeholder='password' name='password' type="password" {...register('password')} />
+				{passwordError && <div className='error'>{passwordError}</div>}
+
+				<input ref={register} placeholder='passwordConfirm' name='passwordConfirm' type="password" {...register('passwordConfirm')} />
+				{passwordConfirmError && <div className='error'>{passwordConfirmError}</div>}
+
+				<button disabled={passwordError || passwordConfirmError || emailError} >Зарегестрироваться</button>
 			</form>
-			<div className="errors">
-				{errorsInput}
-			</div>
-		</div>
+		</div >
 	);
 }
 
 export default App;
-
